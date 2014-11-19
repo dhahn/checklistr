889ba4d13c2e5dce64b7bec6fa3fd701725955c6
David Hahn <davidmichaelhahn@gmail.com>
2014-11-19 13:53:47 -0600

    Add stuf

--- app/controllers/beacons_controller.rb
+++ app/controllers/beacons_controller.rb
@@ -5,7 +5,12 @@ class BeaconsController < ApplicationController
 
 	def person_enters
 		beacon = Beacon.where(major: params[:major], minor: params[:minor]).first
-		beacon.people << Person.where(beacon_id: beacon.id, name: params[:name]).first_or_create
+		person = Person.where(beacon_id: beacon.id, name: params[:name]).first_or_initialize
+		#distnace in meters	
+		person.distance = params[:distance]
+		person.save
+
+		beacon.people << person
 
 		respond_to do |format|
 		  format.json { render json: true }
